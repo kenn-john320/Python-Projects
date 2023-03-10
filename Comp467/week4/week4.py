96213ae2769acf63c43d90da1a76bfe1ac3d12f4
Kennedy Ashley Johnson <kenn.john320@gmail.com>
2023-03-10 00:23:52 -0800

    added in week4 project

--- Comp467/week4/week4.py
+++ Comp467/week4/week4.py
@@ -3,6 +3,8 @@ file = open('lesson4.txt', 'r').readlines()
 for f in file:
     currLine = (f.rstrip("\n"))
     if currLine.__contains__(" "):
-        print(f'File #{f} has an error')
+        print(f'File: {currLine} has an error')
+        print(f'The file will now be fixed:')
+        print(f'{currLine.replace(" ","")}')
     else:
         print(f'File #{f} is fine and has no errors.')
