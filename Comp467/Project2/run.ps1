8c6d1457785c7f494f3e35e5ffc2af3b286818aa
Kennedy Ashley Johnson <kenn.john320@gmail.com>
2023-04-30 15:00:22 -0700

    Co-authored-by: Andres <Knishmas@users.noreply.github.com>

--- Comp467/Project2/run.ps1
+++
@@ -1,5 +0,0 @@
-python script.py --files Baselight_JJacobs_20230323.txt Flame_MFelix_20230323.txt Flame_DFlowers_20230323.txt --xytech Xytech_20230323.txt --verbose 
-python script.py --files Baselight_TDanza_20230324.txt --xytech Xytech_20230324.txt --verbose
-python script.py --files Baselight_GLopez_20230325.txt --xytech Xytech_20230325.txt --verbose 
-python script.py --files Baselight_THolland_20230326.txt Flame_DFlowers_20230326.txt --xytech Xytech_20230326.txt --verbose 
-python script.py --files Baselight_THolland_20230327.txt Flame_DFlowers_20230327.txt --xytech Xytech_20230327.txt --verbose 
--- Comp467/week2/week2.py
+++
@@ -1,8 +0,0 @@
-import re
-
-f = open("ingest_this.txt", "r")
-words = f.readlines()
-vowels = "[aeiouAEIOU]"
-
-for character in words:
-    print(re.sub(vowels, "9", character))
